db75a7131863b66ba6e1fd491128030e6f41ab40
DnKler <57763233+DnKler@users.noreply.github.com>
2021-05-04 18:44:35 +0300

    Update create_wl files

--- static/dynamic_form.js
+++ static/dynamic_form.js
@@ -3,24 +3,30 @@ document.getElementById("addDynamicExtraFieldButton").onclick = function(event) 
 	let div = document.createElement("div");
 	div.className = "DynamicExtraField";
 	$("<br>").appendTo(div);
+	$('<hr>').appendTo(div);
+	$('<br>').appendTo(div);
     $('<input/>', {value : 'Remove item', type : 'button', 'class' : 'DeleteDynamicExtraField'}).appendTo(div).click(function(e) {
 		$(this).parent().remove();
 		e.preventDefault();
 		return false;
-    });  
-    $('<br>').appendTo(div);
+	});  
+	$('<br>').appendTo(div);
+	$('<br>').appendTo(div);
 	$('<p>').appendTo(div);
-	$('<label/>', {for : "item"}).html("Item name:").appendTo(div); //	<label for="item">Item name:</label>
+	$('<label/>', {for : "item"}).html("Item name ").appendTo(div); //	<label for="item">Item name:</label>
 	$('<input/>', {name : 'item-title[]', type : 'text', id : 'item', required :''}).appendTo(div);// <input type="text" name="item-title[]" id="item">
-	$('<label/>', {for : "link"}).html(" Reference:").appendTo(div); //	<label for="link">Reference:</label>
-	$('<input/>', {name : 'item-link[]', type : 'text', placeholder : "Input link to store", id : 'link' }).appendTo(div);	// <input type="text" name="item-link[]" id="link" placeholder="Input link to store">
+	$('<label/>', {for : "link"}).html(" Reference: ").appendTo(div); //	<label for="link">Reference:</label>
+	$('<input/>', {name : 'item-link[]', type : 'text', placeholder : " Link to shop website", id : 'link' }).appendTo(div);	// <input type="text" name="item-link[]" id="link" placeholder="Input link to store">
+	$('</p>').appendTo(div);
+	$('<p>', {class: 'textform'}).html("Input description for your new item").appendTo(div);
 	$('</p>').appendTo(div);
     $('<p>').appendTo(div);
-	$('<textarea/>', {name : 'item-descr[]', placeholder : "Input description for your new item", cols : '50', rows : '3' }).appendTo(div);
-	$('<input/>', {name : 'file[]', type : 'file' }).appendTo(div); // <input type="file" name="file[]">
+	$('<textarea/>', {name : 'item-descr[]', cols : '50' }).appendTo(div);
+	$('<br>').appendTo(div);
+	$('<input/>', {name : 'file[]', type : 'file', class: 'forma'}).appendTo(div); // <input type="file" name="file[]">
 	$('</p>').appendTo(div);
+	
  //Добавляем уже собранный DIV в DynamicExtraFieldsContainer
-    $(div).appendTo("#DynamicExtraFieldsContainer");
+	$(div).appendTo("#DynamicExtraFieldsContainer");
+	
 };
-
-
